250f3fb3c886d5678c97722906193a22e2131467
harshik0007 <tantiharshik@gmail.com>
2026-08-06 20:57:03 +0530

    First Model listings

---
+++ models/listing.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const listingSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1781882915155-2a5355116a74?q=80&w=1227&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1781882915155-2a5355116a74?q=80&w=1227&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        : v,
+  },
+  price: {
+    type: Number,
+  },
+  location: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
