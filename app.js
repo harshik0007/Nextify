f203fcbbea6e685827aac6417b159674994c7d57
harshik0007 <tantiharshik@gmail.com>
2026-08-10 21:45:11 +0530

    Delete route created

--- app.js
+++ app.js
@@ -36,6 +36,13 @@ app.get("/listings/:id/edit", async (req, res) => {
 app.put("/listings/:id", async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
   res.redirect("/listings");
 });
 
