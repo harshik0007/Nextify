dd702b06ac8cfd1ca68bd1a61238cdb86aa473a6
harshik0007 <tantiharshik@gmail.com>
2026-09-03 19:29:42 +0530

    Review model created

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review.js");
 
 const listingSchema = new mongoose.Schema({
   title: {
@@ -17,6 +18,12 @@ const listingSchema = new mongoose.Schema({
         ? "https://images.unsplash.com/photo-1781882915155-2a5355116a74?q=80&w=1227&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         : v,
   },
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Review"
+    }
+  ],
   price: {
     type: Number,
   },
