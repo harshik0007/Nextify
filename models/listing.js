4e3d8c8d398fec8bc7212163c8a151eba712af5b
harshik0007 <tantiharshik@gmail.com>
2026-09-04 00:43:22 +0530

    delete handling done

--- models/listing.js
+++ models/listing.js
@@ -35,5 +35,11 @@ const listingSchema = new mongoose.Schema({
   },
 });
 
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
+})
+
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
