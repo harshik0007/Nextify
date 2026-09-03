f07d715caa4abf6b1a6b36959bf17d60c62e3f9f
harshik0007 <tantiharshik@gmail.com>
2026-09-04 03:37:50 +0530

    router implemented for reviews to make clean layout in app.js for better understanding of code and maintaining clean layout and mergeParams make true for get all things

--- app.js
+++ app.js
@@ -3,14 +3,10 @@ const app = express();
 const methodOverride = require("method-override");
 const path = require("path");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { reviewSchema } = require("./schema.js");
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
-const Review = require("./models/review.js");
-
 const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/nextify";
 
@@ -37,45 +33,8 @@ app.get("/", (req, res) => {
   res.redirect("/listings");
 });
 
-
-
-const validateReview = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-}
-
 app.use("/listings", listings);
-
-//reviews
-//post for a review
-app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res, next) => {
-  let { id } = req.params;
-  let listing = await Listing.findById(id);
-  let newReview = new Review(req.body.review);
-
-  listing.reviews.push(newReview);
-
-  await newReview.save();
-  await listing.save();
-
-  res.redirect(`/listings/${id}`);
-
-}));
-
-//delete a review
-app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res, next) => {
-  let { id, reviewId } = req.params;
-
-  await Listing.findOneAndUpdate({ id: `${id}` }, { $pull: { reviews: reviewId } })
-  await Review.findByIdAndDelete(reviewId);
-
-  res.redirect(`/listings/${id}`);
-}))
+app.use("/listings/:id/reviews", reviews);
 
 app.all("/{*splat}", (req, res, next) => {
   next(new ExpressError(404, "Page Note Found!"));
