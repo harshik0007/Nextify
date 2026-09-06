acc52ec94d59e45ee7b54c79f47824a1a35241bc
harshik0007 <tantiharshik@gmail.com>
2026-09-06 19:30:35 +0530

    login page created and style added

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const User = require("../models/user");
 const router = express.Router({ mergeParams: true });
-const wrapAsync = require("../utils/wrapAsync.js")
+const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
 
 router.get("/signup", (req, res, next) => {
     res.render("users/signup.ejs");
@@ -9,7 +10,7 @@ router.get("/signup", (req, res, next) => {
 
 router.post("/signup", wrapAsync(async (req, res, next) => { //by wrap we redirect to error page but we want flash msg so we use try-catch
     try {
-        const { username, email, password } = req.body.user;
+        const { username, email, password } = req.body;
         const newUser = await new User({ username, email });
         const registeredUser = await User.register(newUser, password);
         console.log(registeredUser);
@@ -20,5 +21,14 @@ router.post("/signup", wrapAsync(async (req, res, next) => { //by wrap we redire
         res.redirect("/signup")
     }
 }));
+
+router.get("/login", (req, res, next) => {
+    res.render("users/login.ejs");
+})
+
+router.post("/login", passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), async (req, res, next) => {
+    req.flash("success", "Welcome back to Nextify!");
+    res.redirect("/listings");
+})
 
 module.exports = router;
