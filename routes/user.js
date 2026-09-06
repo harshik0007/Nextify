14942249775d15fecdb10922a95667bb9b5a6552
harshik0007 <tantiharshik@gmail.com>
2026-09-06 21:40:01 +0530

    logout added

--- routes/user.js
+++ routes/user.js
@@ -31,4 +31,15 @@ router.post("/login", passport.authenticate("local", { failureRedirect: "/login"
     res.redirect("/listings");
 })
 
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+    });
+
+    req.flash("success", "Successfully Logout!");
+    res.redirect("/listings");
+})
+
 module.exports = router;
